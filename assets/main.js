83cfa6051941d8b4d2727cbf40dcd104aba94e56
LeidyRosmery <rosmery.maldonado89@gmail.com>
2017-12-03 19:24:47 -0500

    ultimo commit

--- assets/main.js
+++ assets/main.js
@@ -1,50 +1,49 @@
-$(document).ready(function() {
-  $.ajax({
-    url: "assets/data.csv",
-  }).done(function(res) {
-    var rows = res.split("\n");
-    $.each(rows, function(key, val) {
-      var cols = val.split(",");
+$(document).ready(function(){
+	$.ajax({
+		url:"assets/data.csv",
+	}).done(function(res){
+		var rows = res.split("\n");
+		$.each(rows,function(key,val){
+			var cols = val.split(",");
 
-      var html = '<div class="row">';
+			var html = '<div class="row">';
 
-      $.each(cols, function(key, val) {
-        if (key > 0) html += '<div class="col">' + val + '</div>';
-      });
+			$.each(cols,function(key,val){
+				if(key>0) html+='<div class="col">'+val+'</div>';
+			});
 
-      html += '<div class="clear"></div></div>';
+			html+='<div class="clear"></div></div>';
 
-      $("#win .lista.l" + cols[0]).append(html);
-
-
-    });
+			$("#win .lista.l"+cols[0]).append(html);
 
 
-  })
-  $("#win .select .tab").click(function() {
-    if (!$(this).hasClass("ac")) {
-      $("#win .select .tab").removeClass("ac");
-      $(this).addClass("ac");
+		});
 
-      var ind = $("#win .select .tab").index($(this));
-      $("#win .lista").hide();
-      $("#win .lista").eq(ind).show();
-    }
-  })
-	$(window).scroll(function() {
-            clearTimeout($.data(this, 'scrollTimer'));
-            $.data(this, 'scrollTimer', setTimeout(function() {
-                var topWindow = $(window).scrollTop();
-                if(topWindow>=$('.ventaIntro').position().top && topWindow<$('.ventaDespacho').position().top-150){
-                    $('.groupMenu ul li').removeClass('active');
-                    $('.groupMenu ul li').eq(0).addClass('active');
-                }else if(topWindow>=$('.ventaDespacho').position().top-150 && topWindow<$('.ventaPreguntas').position().top-150){
-                    $('.groupMenu ul li').removeClass('active');
-                    $('.groupMenu ul li').eq(1).addClass('active');
-                }else if(topWindow>=$('.ventaPreguntas').position().top-150){
-                    $('.groupMenu ul li').removeClass('active');
-                    $('.groupMenu ul li').eq(2).addClass('active');
-                }
-            },50));
-        });
+
+	})
+	$("#win .select .tab").click(function(){
+		if(!$(this).hasClass("ac")){
+			$("#win .select .tab").removeClass("ac");
+			$(this).addClass("ac");
+			var ind = $("#win .select .tab").index($(this));
+			$("#win .lista").hide();
+			$("#win .lista").eq(ind).show();
+		}
+	})
+/*
+$('.pregunta').on('click',function(){
+$('.pregunta').remove('active');
+$(this).add('active');
+$(this).css('maxHeight','')
+
+	this.classList.toggle("active");
+
+	 var panel = this.nextElementSibling;
+	 if (panel.style.maxHeight){
+		 panel.style.maxHeight = null;
+	 } else {
+		 panel.style.maxHeight = panel.scrollHeight + "px";
+	 }
+*/
+
 });
