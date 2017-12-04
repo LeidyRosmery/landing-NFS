d0934aef308df6e9e7b163b33f46d604302a23be
LeidyRosmery <rosmery.maldonado89@gmail.com>
2017-12-04 11:40:08 -0500

    fonts

--- assets/main.js
+++ assets/main.js
@@ -1,49 +1,32 @@
-$(document).ready(function(){
-	$.ajax({
-		url:"assets/data.csv",
-	}).done(function(res){
-		var rows = res.split("\n");
-		$.each(rows,function(key,val){
-			var cols = val.split(",");
+$(document).ready(function() {
+  $.ajax({
+    url: "assets/data.csv",
+  }).done(function(res) {
+    var rows = res.split("\n");
+    $.each(rows, function(key, val) {
+      var cols = val.split(",");
 
-			var html = '<div class="row">';
+      var html = '<div class="row">';
 
-			$.each(cols,function(key,val){
-				if(key>0) html+='<div class="col">'+val+'</div>';
-			});
+      $.each(cols, function(key, val) {
+        if (key > 0) html += '<div class="col">' + val + '</div>';
+      });
 
-			html+='<div class="clear"></div></div>';
+      html += '<div class="clear"></div></div>';
 
-			$("#win .lista.l"+cols[0]).append(html);
+      $("#win .lista.l" + cols[0]).append(html);
+    });
 
+  })
+  $("#win .select .tab").click(function() {
+    if (!$(this).hasClass("ac")) {
+      $("#win .select .tab").removeClass("ac");
+      $(this).addClass("ac");
+      var ind = $("#win .select .tab").index($(this));
+      $("#win .lista").hide();
+      $("#win .lista").eq(ind).show();
+    }
+  })
 
-		});
-
-
-	})
-	$("#win .select .tab").click(function(){
-		if(!$(this).hasClass("ac")){
-			$("#win .select .tab").removeClass("ac");
-			$(this).addClass("ac");
-			var ind = $("#win .select .tab").index($(this));
-			$("#win .lista").hide();
-			$("#win .lista").eq(ind).show();
-		}
-	})
-/*
-$('.pregunta').on('click',function(){
-$('.pregunta').remove('active');
-$(this).add('active');
-$(this).css('maxHeight','')
-
-	this.classList.toggle("active");
-
-	 var panel = this.nextElementSibling;
-	 if (panel.style.maxHeight){
-		 panel.style.maxHeight = null;
-	 } else {
-		 panel.style.maxHeight = panel.scrollHeight + "px";
-	 }
-*/
 
 });
