8d33f0513ba09e75e41b1c1c0bbf4490804a35cd
LeidyRosmery <rosmery.maldonado89@gmail.com>
2017-12-04 12:23:38 -0500

    add icon plus and minus

--- assets/main.js
+++ assets/main.js
@@ -27,6 +27,19 @@ $(document).ready(function() {
       $("#win .lista").eq(ind).show();
     }
   })
+  $('.pregunta').on('click', function() {
+    console.log("clikeame");
 
+
+  });
+  $('.collapse').on('show.bs.collapse', function() {
+    console.log("abierto");
+    $(this).siblings('div').find('i').removeClass('glyphicon glyphicon-plus');
+		$(this).siblings('div').find('i').addClass('glyphicon glyphicon-minus');
+  })
+  $('.collapse').on('hide.bs.collapse', function() {
+		$(this).siblings('div').find('i').removeClass('glyphicon glyphicon-minus');
+		$(this).siblings('div').find('i').addClass('glyphicon glyphicon-plus');
+  })
 
 });
